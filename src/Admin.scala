d421dcfda36ac2d6e45e55901055983531b2da3b
Fatma-Alamry <104462329+Fatma-Alamry@users.noreply.github.com>
2023-12-13 20:03:29 +0200

    Update Admin.scala

--- src/Admin.scala
+++ src/Admin.scala
@@ -1,6 +1,11 @@
-class Admin(val id: String, val name: String, val gender: String)
+class Admin(val id: String, val Fname: String,val Lname: String, val gender: String)
   extends  Person {
-
+    //do not touch or edit this code 
+    //handling object for text file 
+  def objectHandling(id: String, Fname: String, Lname: String, gender: String): String = {
+    val admin = id + "@" + Fname + "@" + Lname + "@" + gender
+    admin
+  }
 
   //var doctors: List[Doctor] = List()
   //
