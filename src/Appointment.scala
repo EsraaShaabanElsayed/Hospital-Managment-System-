3e707902f8f573f216f0c8fa2e4fd86008f6006c
Fatma-Alamry <104462329+Fatma-Alamry@users.noreply.github.com>
2023-12-13 19:21:53 +0200

    Update Appointment.scala

--- src/Appointment.scala
+++ src/Appointment.scala
@@ -1,7 +1,7 @@
 class Appointment(val id: String, val patientId:String, val doctorId: String, val date: String, val time: String)
 { 
   // do not touch or edit this code ever never 
-  //handling object for file 
+  //handling object for file for write
   def objectHandling (id:String,patientId:String,doctorId:String,date:String,time: String): String = {
      val appointment= id+"@"+patientId+"@"+doctorId+"@"+date+"@"+time
     appointment
