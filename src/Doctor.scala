2f59e769158bbc546ddf75db9af26450d9a2a9e9
DoniaKaram <105426098+DoniaKaram@users.noreply.github.com>
2023-12-15 17:45:06 +0200

    Update Doctor.scala

--- src/Doctor.scala
+++ src/Doctor.scala
@@ -1,7 +1,68 @@
-import java.time.LocalDateTime
-import scala.collection.mutable.ListBuffer
+import akka.actor.{Actor, ActorLogging, Props}
 
-class Doctor(val name: String,  val gender: String,val id :String , val availability: ListBuffer[LocalDateTime], val specialization: String)
-  extends Person {
+class DoctorActor(val id: Int, val name: String, val specialty: String) extends Actor with ActorLogging {
+  import DoctorActor._
 
+  private var schedule: Map[String, String] = Map.empty
+  private var patientHistory: Map[String, String] = Map.empty
+
+  override def receive: Receive = {
+    case ScheduleCheckup(date, patientId) =>
+      scheduleCheckup(date, patientId)
+      sender() ! CheckupScheduled
+
+    case ViewSchedule(date) =>
+      sender() ! ScheduleView(viewSchedule(date))
+
+    case ViewPatientHistory(patientId) =>
+      sender() ! PatientHistoryView(viewPatientHistory(patientId))
+
+    case ConductCheckup(patientId, diagnosis, medication) =>
+      conductCheckup(patientId, diagnosis, medication)
+      sender() ! CheckupConducted
+
+    case CancelCheckup(date) =>
+      cancelCheckup(date)
+      sender() ! CheckupCancelled
+  }
+
+  private def scheduleCheckup(date: String, patientId: String): Unit = {
+    schedule += (date -> patientId)
+  }
+
+  private def viewSchedule(date: String): Option[String] = {
+    schedule.get(date)
+  }
+
+  private def viewPatientHistory(patientId: String): Option[String] = {
+    patientHistory.get(patientId)
+  }
+
+  private def conductCheckup(patientId: String, diagnosis: String, medication: String): Unit = {
+    val prescription = s"Diagnosis: $diagnosis, Medication: $medication"
+    patientHistory += (patientId -> prescription)
+  }
+
+  private def cancelCheckup(date: String): Unit = {
+    schedule -= date
+  }
+}
+
+object DoctorActor {
+  // Commands
+  case class ScheduleCheckup(date: String, patientId: String)
+  case class ViewSchedule(date: String)
+  case class ViewPatientHistory(patientId: String)
+  case class ConductCheckup(patientId: String, diagnosis: String, medication: String)
+  case class CancelCheckup(date: String)
+
+  // Responses
+  case object CheckupScheduled
+  case class ScheduleView(patientId: Option[String])
+  case class PatientHistoryView(history: Option[String])
+  case object CheckupConducted
+  case object CheckupCancelled
+
+  def props(id: Int, name: String, specialty: String): Props =
+    Props(new DoctorActor(id, name, specialty))
 }
