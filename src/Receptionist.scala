5c885ede73677fb4e16ccaecb675f1061c3ae187
Esraa Shaaban <85762396+EsraaShaabanElsayed@users.noreply.github.com>
2023-12-16 16:12:04 +0200

    Update Receptionist.scala
    crud opertion in Appointment

--- src/Receptionist.scala
+++ src/Receptionist.scala
@@ -1,11 +1,19 @@
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+
 case class Receptionist(id: Int, name: String, specialization: String)
 case class AddReceptionist(receptionist: Receptionist)
 case class GetReceptionist(id: Int)
 case class UpdateReceptionist(receptionist: Receptionist)
 case class RemoveReceptionist(id: Int)
-class ReceptionistActor(patientActor: ActorRef) extends Actor with ActorLogging {
+
+// New Appointment-related messages
+case class ScheduleAppointment(patientUsername: String, doctorUsername: String, day: String)
+case class CancelAppointment(patientUsername: String, doctorUsername: String, day: String)
+case object GetAppointments
+
+class ReceptionistActor(patientActor: ActorRef, appointmentActor: ActorRef) extends Actor with ActorLogging {
   var receptionists: Map[Int, Receptionist] = Map.empty
+
   override def receive: Receive = {
     case AddPatient(patient) =>
       patientActor ! AddPatient(patient)
@@ -14,63 +22,61 @@ class ReceptionistActor(patientActor: ActorRef) extends Actor with ActorLogging 
       patientActor ! GetPatient(id)
 
     case UpdatePatient(patient) =>
-      patientActor! UpdatePatient(patient)
+      patientActor ! UpdatePatient(patient)
 
     case RemovePatient(id) =>
       patientActor ! RemovePatient(id)
-    case AddReceptionist(receptionist)
-    =>
-      receptionists += (receptionist.id -> receptionist)
-      log.info(s"Doctor added: $receptionist")
 
-    case GetReceptionist(id)
-    =>
+    case AddReceptionist(receptionist) =>
+      receptionists += (receptionist.id -> receptionist)
+      log.info(s"Receptionist added: $receptionist")
+
+    case GetReceptionist(id) =>
       sender() ! receptionists.get(id)
 
-    case UpdateReceptionist(updatedReceptionist)
-    =>
+    case UpdateReceptionist(updatedReceptionist) =>
       receptionists.get(updatedReceptionist.id) match {
         case Some(_) =>
           receptionists += (updatedReceptionist.id -> updatedReceptionist)
-          log.info(s"Doctor updated: $updatedReceptionist")
+          log.info(s"Receptionist updated: $updatedReceptionist")
         case None =>
-          log.warning(s"Doctor with ID ${updatedReceptionist.id} not found.")
+          log.warning(s"Receptionist with ID ${updatedReceptionist.id} not found.")
       }
 
-    case RemoveReceptionist(id)
-    =>
-      receptionists.get(id) match {
-        case Some(doctor) =>
-          receptionists -= id
-          log.info(s"Doctor removed: $doctor")
-        case None =>
-          log.warning(s"Doctor with ID $id not found.")
-      }
+    // New Appointment-related cases
+    case ScheduleAppointment(patientUsername, doctorUsername, day) =>
+      appointmentActor ! AppointmentActor.ScheduleAppointment(Appointment(patientUsername, doctorUsername, day))
+
+    case CancelAppointment(patientUsername, doctorUsername, day) =>
+      appointmentActor ! AppointmentActor.CancelAppointment(Appointment(patientUsername, doctorUsername, day))
+
+    case GetAppointments =>
+      appointmentActor ! AppointmentActor.GetAppointments
   }
-
-
 }
 
-object PatientApp extends App {
+object HospitalApp extends App {
   val system = ActorSystem("hospitalSystem")
 
-  // Create DoctorRepository actor
+  // Create PatientActor and AppointmentActor
   val patientActor = system.actorOf(Props[patientActor], "patientRepository")
+  val appointmentActor = system.actorOf(Props[AppointmentActor], "appointmentRepository")
 
-  // Create Admin actor and pass DoctorRepository actor reference
-  val admin = system.actorOf(Props(new ReceptionistActor(patientActor)), "admin")
+  // Create ReceptionistActor and pass PatientActor and AppointmentActor references
+  val receptionist = system.actorOf(Props(new ReceptionistActor(patientActor, appointmentActor)), "receptionist")
 
   // Example CRUD operations
-  admin ! AddPatient(Patient(1, "Dr. Smith", "Cardiology"))
-  admin ! AddPatient(Patient(2, "Dr. Johnson", "Orthopedics"))
-  admin ! GetPatient(1)
-  admin ! UpdatePatient(Patient(1, "Dr. Smith Jr.", "Cardiology"))
-  admin ! RemovePatient(2)
+  receptionist ! AddPatient(Patient(1, "Dr. Smith", "Cardiology"))
+  receptionist ! AddPatient(Patient(2, "Dr. Johnson", "Orthopedics"))
+  receptionist ! GetPatient(1)
+  receptionist ! UpdatePatient(Patient(1, "Dr. Smith Jr.", "Cardiology"))
+  receptionist ! RemovePatient(2)
+
+  // Example Appointment operations
+  receptionist ! ScheduleAppointment("patient1", "doctor1", "2023-01-01")
+  receptionist ! CancelAppointment("patient1", "doctor1", "2023-01-01")
+  receptionist ! GetAppointments
 
   // Shut down the actor system
-  system.terminate()
-}
-
-  // Shutdown the system
   system.terminate()
 }
