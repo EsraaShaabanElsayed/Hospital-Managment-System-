a54dffcdc2974f64da913c3c5d6b9fed21c78e80
DoniaKaram <105426098+DoniaKaram@users.noreply.github.com>
2023-12-16 11:26:14 +0200

    Update Admin.scala

--- src/Admin.scala
+++ src/Admin.scala
@@ -1,107 +1,59 @@
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 
-case class Doctor(id: Int, name: String, specialization: String)
-case class Receptionist(id: Int, name: String)
-class Admin extends Actor with ActorLogging {
+// Admin actor to interact with DoctorRepository
+class Admin(doctorActor: ActorRef,receptionistActor: ActorRef) extends Actor with ActorLogging {
+  override def receive: Receive = {
+    case AddDoctor(doctor) =>
+      doctorActor ! AddDoctor(doctor)
 
-  var doctors: List[Doctor] = loadDoctorsFromFile()
-  var receptionist: List[Receptionist] = loadReceptionistFromFile()
-  def receive: Receive = {
-    case GetReceptionist => sender() ! receptionist
-    case AddReceptionist(receptionist) => addReceptionist(receptionist)
-    case UpdateReceptionist(receptionist) => updateReceptionist(receptionist)
-    case RemoveReceptionist(id) => removeReceptionist(id)
-    case GetDoctors => sender() ! doctors
-    case AddDoctor(doctor) => addDoctor(doctor)
-    case UpdateDoctor(doctor) => updateDoctor(doctor)
-    case RemoveDoctor(id) => removeDoctor(id)
-  }
-  //read all receptionists from file 
-def loadReceptionistFromFile(): List[Receptionist] = {
-    // Implementation to read doctors from a file
-    // This can be a simple CSV, JSON, or any format suitable for your application
-    // Return a list of Doctor objects
-    // Example: List(Doctor(1, "Dr. Smith", "Cardiology"), Doctor(2, "Dr. Johnson", "Pediatrics"))
-    // ...
-  }
-//read all doctors from file
-  def loadDoctorsFromFile(): List[Doctor] = {
-    // Implementation to read doctors from a file
-    // This can be a simple CSV, JSON, or any format suitable for your application
-    // Return a list of Doctor objects
-    // Example: List(Doctor(1, "Dr. Smith", "Cardiology"), Doctor(2, "Dr. Johnson", "Pediatrics"))
-    // ...
-  }
-def saveReceptionistsToFile(): Unit = {
-    // Implementation to save doctors to a file
-    // This should write the current state of the 'doctors' list to a file
-    // ...
-  }
-  def saveDoctorsToFile(): Unit = {
-    // Implementation to save doctors to a file
-    // This should write the current state of the 'doctors' list to a file
-    // ...
-  }
- def addReceptionist(receptionist: Receptionist): Unit = {
-    receptionists = Receptionists :+ receptionist
-    saveReceptionistsToFile()
-  }
-  def addDoctor(doctor: Doctor): Unit = {
-    doctors = doctors :+ doctor
-    saveDoctorsToFile()
-  }
-   def updateReceptionist(updatedReceptionist: Receptionist): Unit = {
-    receptionists.find(_.id == updatedReceptionist.id) match {
-      case Some(existingReceptionist) =>
-        receptionists = receptionists.map {
-          case `existingReceptionist` => updatedReceptionist
-          case otherReceptionist => otherReceptionist
-        }
-        saveReceptionistsToFile()
-      case None => log.warning(s"Receptionist with ID ${updatedReceptionist.id} not found.")
-    }
+    case GetDoctor(id) =>
+      doctorActor ! GetDoctor(id)
+
+    case UpdateDoctor(doctor) =>
+      doctorActor ! UpdateDoctor(doctor)
+
+    case RemoveDoctor(id) =>
+      doctorActor ! RemoveDoctor(id)
+    case AddReceptionist(receptionist)
+    =>
+      doctorActor ! AddReceptionist(receptionist)
+
+    case GetReceptionist(id)
+    =>
+      receptionistActor ! GetReceptionist(id)
+
+    case UpdateReceptionist(receptionist)
+    =>
+      receptionistActor ! UpdateReceptionist(receptionist)
+
+    case RemoveReceptionist(id)
+    =>
+      receptionistActor ! RemoveReceptionist(id)
   }
 
-  def updateDoctor(updatedDoctor: Doctor): Unit = {
-    doctors.find(_.id == updatedDoctor.id) match {
-      case Some(existingDoctor) =>
-        doctors = doctors.map {
-          case `existingDoctor` => updatedDoctor
-          case otherDoctor => otherDoctor
-        }
-        saveDoctorsToFile()
-      case None => log.warning(s"Doctor with ID ${updatedDoctor.id} not found.")
-    }
-  }
-  def removeReceptionist(receptionistId: Int): Unit = {
-    receptionists.find(_.id == receptionistId) match {
-      case Some(_) =>
-        receptionists = receptionists.filterNot(_.id == receptionistId)
-       saveReceptionistsToFile()
-      case None => log.warning("Receptionist with ID $receptionistId not found.")
-    }
-  }
 
-  def removeDoctor(doctorId: Int): Unit = {
-    doctors.find(_.id == doctorId) match {
-      case Some(_) =>
-        doctors = doctors.filterNot(_.id == doctorId)
-        saveDoctorsToFile()
-      case None => log.warning(s"Doctor with ID $doctorId not found.")
-    }
-  }
+
 }
 
-object Admin {
-  def props: Props = Props[Admin]
-}
+object AdminApp extends App {
+  val system = ActorSystem("hospitalSystem")
 
-// Messages
-case object GetDoctors
-case class AddDoctor(doctor: Doctor)
-case class UpdateDoctor(doctor: Doctor)
-case class RemoveDoctor(id: Int)
-case object GetReceptionist
-case class AddReceptionist(receptionist:Receptionist)
-case class UpdateReceptionist(receptionist:Receptionist)
-case class RemoveReceptionist(id: Int)
+  // Create DoctorRepository actor
+  val doctorActor = system.actorOf(Props[DoctorActor], "doctorRepository")
+  val receptionistActor = system.actorOf(Props[ReceptionistActor], "receptionistRepository")
+  // Create Admin actor and pass DoctorRepository actor reference
+  val admin = system.actorOf(Props(new Admin(doctorActor,receptionistActor)), "admin")
+  // Example CRUD operations
+  admin ! AddDoctor(Doctor(1, "Dr. Smith", "Cardiology"))
+  admin! AddDoctor(Doctor(2, "Dr. Johnson", "Orthopedics"))
+  admin! GetDoctor(1)
+  admin ! UpdateDoctor(Doctor(1, "Dr. Smith Jr.", "Cardiology"))
+  admin! RemoveDoctor(2)
+  admin ! AddReceptionist(Receptionist(1, "Dr. Smith", "Cardiology"))
+  admin ! AddReceptionist(Receptionist(2, "Dr. Johnson", "Orthopedics"))
+  admin ! GetReceptionist(1)
+  admin ! UpdateReceptionist(Receptionist(1, "Dr. Smith Jr.", "Cardiology"))
+  admin ! RemoveReceptionist(2)
+  // Shut down the actor system
+  system.terminate()
+}
