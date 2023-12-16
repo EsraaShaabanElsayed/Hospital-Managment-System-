3526da38317c2d6c9b180a9f51cd08a3f35586d3
Esraa Shaaban <85762396+EsraaShaabanElsayed@users.noreply.github.com>
2023-12-16 20:34:00 +0200

    Update Patient.scala

--- src/Patient.scala
+++ src/Patient.scala
@@ -1,42 +1,45 @@
 import akka.actor.{Actor, ActorLogging}
 
 // Doctor class
-case class Patient(username: String, email: String,specialization: String)
-
+case class Patient( Fname: String, LName: String, gender: String,email:String,Username:String)
 // Messages for CRUD operations
 case class AddPatient(patient: Patient)
-case class GetPatient(username: String)
+case class GetPatient(Username: String)
 case class UpdatePatient(patient: Patient)
-case class RemovePatient(username:String)
+case class RemovePatient(username: String)
 
 // DoctorRepository actor to manage doctors
 class patientActor extends Actor with ActorLogging {
-  var patients: Map[Int, Patient] = Map.empty
+  val filename = "patient.txt"
+  var patients: Map[String, Patient] = Map.empty
+  private def objectHandling(r: Patient): String = {
+    s"${r.Fname},${r.LName},${r.gender},${r.email},${r.Username}"
+  }
 
   override def receive: Receive = {
     case AddPatient(patient) =>
-      patients += (patient.id -> patient)
+      patients += (patient.Username-> patient)
       log.info(s"Patient added: $patient")
 
-    case GetPatient(username) =>
-      sender() ! patients.get(username)
+    case GetPatient(username: String) =>
+      sender() ! patients.get(username: String)
 
     case UpdatePatient(updatedPatient) =>
-      patients.get(updatedPatient.id) match {
+      patients.get(updatedPatient.Username) match {
         case Some(_) =>
-          patients += (updatedPatient.id ->updatedPatient)
+          patients += (updatedPatient.Username ->updatedPatient)
           log.info(s"Patient updated: $updatedPatient")
         case None =>
-          log.warning(s"Patient with ID ${updatedPatient.id} not found.")
+          log.warning(s"Patient with Username ${updatedPatient.Username} not found.")
       }
 
     case RemovePatient(username) =>
       patients.get(username) match {
         case Some(doctor) =>
-          patients -= username 
+          patients -= username
           log.info(s"Patient removed: $doctor")
         case None =>
-          log.warning(s"Patient with ID $id not found.")
+          log.warning(s"Patient with Username $username not found.")
       }
   }
 }
