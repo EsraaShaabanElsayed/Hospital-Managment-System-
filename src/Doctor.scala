587b87ea8e71de47cfb5f49e92135591c8cfb644
Esraa Shaaban <85762396+EsraaShaabanElsayed@users.noreply.github.com>
2023-12-16 21:19:56 +0200

    Update Doctor.scala

--- src/Doctor.scala
+++ src/Doctor.scala
@@ -1,11 +1,8 @@
 import akka.actor.{Actor, ActorLogging}
 
 // Doctor class
-case class Doctor(username: String,email: String, password: String, specialization: String)
 
- 
-
-
+case class Doctor( Fname: String, LName: String, gender: String,email:String,password:String,Username:String,specialization: String)
 // Messages for CRUD operations
 case class AddDoctor(doctor: Doctor)
 case class GetDoctor(username: String)
@@ -14,35 +11,36 @@ case class RemoveDoctor(username: String)
 
 // DoctorRepository actor to manage doctors
 class DoctorActor extends Actor with ActorLogging {
-  var doctors: Map[Int, Doctor] = Map.empty
-
+  var doctors: Map[ String, Doctor] = Map.empty
+  val filename = "doctor.txt"
+  private def objectHandling(r: Doctor): String = {
+    s"${r.Fname},${r.LName},${r.gender},${r.email},${r.password},${r.Username},${r.specialization}"
+  }
   override def receive: Receive = {
     case AddDoctor(doctor) =>
-      doctors += (doctor.id -> doctor)
+      doctors += (doctor.Username -> doctor)
       log.info(s"Doctor added: $doctor")
 
     case GetDoctor(username) =>
       sender() ! doctors.get(username)
 
     case UpdateDoctor(updatedDoctor) =>
-      doctors.get(updatedDoctor.id) match {
+      doctors.get(updatedDoctor.Username) match {
         case Some(_) =>
-          doctors += (updatedDoctor.id -> updatedDoctor)
+          doctors += (updatedDoctor.Username-> updatedDoctor)
           log.info(s"Doctor updated: $updatedDoctor")
         case None =>
-          log.warning(s"Doctor with ID ${updatedDoctor.id} not found.")
+          log.warning(s"Doctor with Username ${updatedDoctor.Username} not found.")
       }
 
     case RemoveDoctor(username) =>
       doctors.get(username) match {
         case Some(doctor) =>
-          doctors -= username 
+          doctors -= username
           log.info(s"Doctor removed: $doctor")
         case None =>
-          log.warning(s"Doctor with ID $id not found.")
+          log.warning(s"Doctor with Username $username not found.")
       }
 
   }
 }
-
-
