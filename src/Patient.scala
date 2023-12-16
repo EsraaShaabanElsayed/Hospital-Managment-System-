f311267384c44cf74f6a0a82e33707b0e7988c60
DoniaKaram <105426098+DoniaKaram@users.noreply.github.com>
2023-12-16 11:27:52 +0200

    Update Patient.scala

--- src/Patient.scala
+++ src/Patient.scala
@@ -1,6 +1,42 @@
-import scala.collection.mutable.ListBuffer
-class Patient(val id: String,  val name: String,gender: String, val diagnoses: ListBuffer[String], val medicalHistory: ListBuffer[String])
-    extends Person{
-  def this(id: String, name: String, gender: String) = this(id, name, gender, ListBuffer(), ListBuffer())
+import akka.actor.{Actor, ActorLogging}
 
+// Doctor class
+case class Patient(id: Int, name: String, specialization: String)
+
+// Messages for CRUD operations
+case class AddPatient(patient: Patient)
+case class GetPatient(id: Int)
+case class UpdatePatient(patient: Patient)
+case class RemovePatient(id: Int)
+
+// DoctorRepository actor to manage doctors
+class patientActor extends Actor with ActorLogging {
+  var patients: Map[Int, Patient] = Map.empty
+
+  override def receive: Receive = {
+    case AddPatient(patient) =>
+      patients += (patient.id -> patient)
+      log.info(s"Patient added: $patient")
+
+    case GetPatient(id) =>
+      sender() ! patients.get(id)
+
+    case UpdatePatient(updatedPatient) =>
+      patients.get(updatedPatient.id) match {
+        case Some(_) =>
+          patients += (updatedPatient.id ->updatedPatient)
+          log.info(s"Patient updated: $updatedPatient")
+        case None =>
+          log.warning(s"Patient with ID ${updatedPatient.id} not found.")
+      }
+
+    case RemovePatient(id) =>
+      patients.get(id) match {
+        case Some(doctor) =>
+          patients -= id
+          log.info(s"Patient removed: $doctor")
+        case None =>
+          log.warning(s"Patient with ID $id not found.")
+      }
+  }
 }
