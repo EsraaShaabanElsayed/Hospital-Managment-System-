53379fe85a7cf2c7b5bf5b05bacf953df6332c65
DoniaKaram <105426098+DoniaKaram@users.noreply.github.com>
2023-12-16 11:26:52 +0200

    Update Doctor.scala

--- src/Doctor.scala
+++ src/Doctor.scala
@@ -1,68 +1,46 @@
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging}
 
-class DoctorActor(val id: Int, val name: String, val specialty: String) extends Actor with ActorLogging {
-  import DoctorActor._
+// Doctor class
+case class Doctor(id: Int, name: String, specialization: String)
 
-  private var schedule: Map[String, String] = Map.empty
-  private var patientHistory: Map[String, String] = Map.empty
+
+// Messages for CRUD operations
+case class AddDoctor(doctor: Doctor)
+case class GetDoctor(id: Int)
+case class UpdateDoctor(doctor: Doctor)
+case class RemoveDoctor(id: Int)
+
+// DoctorRepository actor to manage doctors
+class DoctorActor extends Actor with ActorLogging {
+  var doctors: Map[Int, Doctor] = Map.empty
 
   override def receive: Receive = {
-    case ScheduleCheckup(date, patientId) =>
-      scheduleCheckup(date, patientId)
-      sender() ! CheckupScheduled
+    case AddDoctor(doctor) =>
+      doctors += (doctor.id -> doctor)
+      log.info(s"Doctor added: $doctor")
 
-    case ViewSchedule(date) =>
-      sender() ! ScheduleView(viewSchedule(date))
+    case GetDoctor(id) =>
+      sender() ! doctors.get(id)
 
-    case ViewPatientHistory(patientId) =>
-      sender() ! PatientHistoryView(viewPatientHistory(patientId))
+    case UpdateDoctor(updatedDoctor) =>
+      doctors.get(updatedDoctor.id) match {
+        case Some(_) =>
+          doctors += (updatedDoctor.id -> updatedDoctor)
+          log.info(s"Doctor updated: $updatedDoctor")
+        case None =>
+          log.warning(s"Doctor with ID ${updatedDoctor.id} not found.")
+      }
 
-    case ConductCheckup(patientId, diagnosis, medication) =>
-      conductCheckup(patientId, diagnosis, medication)
-      sender() ! CheckupConducted
+    case RemoveDoctor(id) =>
+      doctors.get(id) match {
+        case Some(doctor) =>
+          doctors -= id
+          log.info(s"Doctor removed: $doctor")
+        case None =>
+          log.warning(s"Doctor with ID $id not found.")
+      }
 
-    case CancelCheckup(date) =>
-      cancelCheckup(date)
-      sender() ! CheckupCancelled
-  }
-
-  private def scheduleCheckup(date: String, patientId: String): Unit = {
-    schedule += (date -> patientId)
-  }
-
-  private def viewSchedule(date: String): Option[String] = {
-    schedule.get(date)
-  }
-
-  private def viewPatientHistory(patientId: String): Option[String] = {
-    patientHistory.get(patientId)
-  }
-
-  private def conductCheckup(patientId: String, diagnosis: String, medication: String): Unit = {
-    val prescription = s"Diagnosis: $diagnosis, Medication: $medication"
-    patientHistory += (patientId -> prescription)
-  }
-
-  private def cancelCheckup(date: String): Unit = {
-    schedule -= date
   }
 }
 
-object DoctorActor {
-  // Commands
-  case class ScheduleCheckup(date: String, patientId: String)
-  case class ViewSchedule(date: String)
-  case class ViewPatientHistory(patientId: String)
-  case class ConductCheckup(patientId: String, diagnosis: String, medication: String)
-  case class CancelCheckup(date: String)
 
-  // Responses
-  case object CheckupScheduled
-  case class ScheduleView(patientId: Option[String])
-  case class PatientHistoryView(history: Option[String])
-  case object CheckupConducted
-  case object CheckupCancelled
-
-  def props(id: Int, name: String, specialty: String): Props =
-    Props(new DoctorActor(id, name, specialty))
-}
